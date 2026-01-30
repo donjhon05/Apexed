994d3580ccc974dfb5df9682761f497192b12708
lgab1219 <lancegabrielmaghanoy@gmail.com>
2026-01-30 22:28:48 +0800

    created: added an index file

---
+++ client/app/page.tsx
@@ -0,0 +1,12 @@
+import Navbar from "./components/Navbar"
+// import Hero from "./components/Hero"
+import Header from "./components/Header"
+
+export default function Home() {
+    return (
+        <main>
+            <Navbar />
+            <Header />
+        </main>
+    )
+}
