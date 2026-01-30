eceec546f16d526c396d5ab865bfa727611c7591
lgab1219 <lancegabrielmaghanoy@gmail.com>
2026-01-30 22:26:43 +0800

    modified: adjusted components' styling

--- client/app/components/Header.tsx
+++ client/app/components/Header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
     return (
         <section className="flex flex-col justify-center text-center items-center my-[5em]
-        gap-y-3">
+        gap-y-3 urbanist">
             <div className="md:w-[20vw] flex-initial p-3 border border-brand-orange text-white rounded-full bg-brand-opaque-orange
             sm:w-[50vw]">
                 <span>
@@ -9,7 +9,7 @@ export default function Header() {
                 </span>
             </div>
             <div>
-                <h1 className="text-3xl md:text-6xl font-semibold mt-4 mt-2 text-white">Automation that works
+                <h1 className="text-3xl md:text-6xl mt-4 mt-2 text-white">Automation that works
                     <span className="italic text-brand-orange italic-serif-semibold"><h1>from day one</h1></span>
                     to every day after.
                 </h1>
