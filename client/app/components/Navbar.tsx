3e75cda2d58f8208434ff3ca4578563fcd69a8f9
lgab1219 <lancegabrielmaghanoy@gmail.com>
2026-02-09 19:39:54 +0800

    modified: reduced padding-y and adjusted border color to be brighter

--- client/app/components/Navbar.tsx
+++ client/app/components/Navbar.tsx
@@ -3,10 +3,10 @@ import Image from "next/image";
 
 function Navbar() {
   return (
-    <header className="relative bg-gray-700/10 flex z-20 items-center justify-between urbanist mx-[8vw] my-8 px-4 py-3 border border-gray-700 rounded-3xl">
+    <header className="glass-navbar relative bg-gray-700/10 flex z-20 items-center justify-between urbanist mx-[8vw] my-8 px-4 py-1 border border-gray-700 rounded-3xl">
       <nav className="hidden md:flex gap-8 text-sm text-zinc-300 items-center justify-center">
         <div className="text-xl font-bold text-brand-orange">
-          <Image src="/assets/images/apexed-logo.webp" width={80} height={80} alt="apexed-logo" />
+          <Image src="/assets/images/apexed-logo.webp" width={70} height={70} alt="apexed-logo" />
         </div>
         <span>Home</span>
         <span>Services</span>
@@ -14,9 +14,9 @@ function Navbar() {
       </nav>
         <section className="flex justify-between items-center gap-8 text-sm">
           <span className="text-white">Login</span>
-          <button className="rounded-lg border border-brand-orange px-6 py-2 bg-brand-orange text-white
-          hover-bg-brand-opaque-dark transition">
-          Request Assessment
+          <button className="glass-button rounded-lg px-6 py-3 text-white
+          hover-bg-brand-orange transition">
+          Start Your Subscription
           </button>
         </section>
     </header>
