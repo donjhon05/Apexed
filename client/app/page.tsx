4c7e90274e7a53f658c90bf1824840f60b43e0d0
lgab1219 <lancegabrielmaghanoy@gmail.com>
2026-02-12 22:43:40 +0800

    modified: changed overflow-x to hidden

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -8,7 +8,7 @@ import Problem from "./components/Problem"
 
 export default function Home() {
     return (
-        <main>
+        <main className="overflow-x-hidden">
             <GlowEffects />
             <BlobEffects />
             <Navbar />
