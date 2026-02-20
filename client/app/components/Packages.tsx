c278aa8fd9c6d16f805bffb9398ad7e9250b15cb
lgab1219 <lancegabrielmaghanoy@gmail.com>
2026-02-20 23:18:56 +0800

    modified: adjusted spacing and added switch button for monthly and yearly content

--- client/app/components/Packages.tsx
+++ client/app/components/Packages.tsx
@@ -1,8 +1,12 @@
 "use client"
 import Image from "next/image"
 import { packages } from "../package-inclusions"
+import { useState } from "react"
 
 export default function Packages() {
+
+    const [yearly, toggleYearly] = useState<boolean>(false);
+
     return (
         <section className="relative flex flex-col justify-center text-center items-center my-[5em]
         gap-y-3 urbanist">
@@ -19,14 +23,25 @@ export default function Packages() {
                         After choosing a service category, select how many workflows you want automated.
                     </p>
                 </span>
+                <span className="glass-button inline-block justify-center py-2 text-center border border-[#1e1e1e] rounded-full"
+                onClick={() => toggleYearly(!yearly)}>
+                    <button className={!yearly ? 
+                    `text-white text-sm py-3 px-4 rounded-full mx-2 bg-[#121212] border border-[#1e1e1e]` :
+                    `text-white text-sm py-3 px-4 mx-2 rounded-full`}>Monthly</button>
+                    <button className={yearly ? 
+                    `text-white text-sm py-3 px-4 rounded-full mx-2 bg-[#121212] border border-[#1e1e1e]` :
+                    `text-white text-sm py-3 px-4 mx-2 rounded-full`}>Yearly</button>
+                </span>
             </div>
                 
-            <div className="flex flex-col md:flex-row justify-center gap-y-5 my-[2em] md:mx-[5vw] text-start z-20">
+            <div className="flex flex-col md:flex-row justify-center gap-y-5 m-[1em] md:gap-x-[15px] text-start z-20">
 
-                <div className="py-3 px-[2vw] mx-[3vw] bg-[#121212] border border-[#1e1e1e] rounded-lg">
+                <div className="py-3 px-[2vw] md:w-[395px] bg-[#121212] border border-[#1e1e1e] rounded-lg">
                     <p className="text-gradient-orange rounded-lg inline-block px-5 font-medium text-center text-lg p-2 border border-[#1e1e1e] my-3">Basic</p>
                     <span>
-                        <h3 className="text-white text-lg font-medium py-3">$446 /month</h3>
+                        {!yearly ? 
+                        <h3 className="text-white text-lg font-medium py-3">$446 /month</h3> : 
+                        <h3 className="text-white text-lg font-medium py-3">$15,000 /yearly</h3>}
                         <p className="text-[#929292] py-3">Essential automation to get your workflows up and running fast.</p>
                     </span>
                     <div className="flex flex-col self-center">
@@ -38,18 +53,22 @@ export default function Packages() {
                     <span>
                         <h3 className="text-white text-lg font-medium py-3">What's included:</h3>
                         {packages.filter((item) => item.type === "basic").map((item) => (
-                            <span key={item.package_id} className="text-[#929292] py-3 text-wrap flex flex-row">
-                                <Image className="p-3 border border-[#1e1e1e]" src="/assets/checkmark.svg" alt="checkmark" width={40} height={40} />
+                            <span key={item.package_id} className="text-[#929292] py-3 text-wrap flex flex-row whitespace-pre-line">
+                                <div className="w-12 h-12 flex items-center justify-center shrink-0 rounded-md border border-[#1e1e1e]">
+                                    <Image src="/assets/checkmark.svg" alt="checkmark" width={20} height={20} />
+                                </div>
                                 <p className="px-3">{item.content}</p>
                             </span>
                         ))}
                     </span>
                 </div>
-                <div className="py-3 px-[2vw] mx-[3vw] bg-[#121212] border border-[#1e1e1e] rounded-lg">
+                <div className="relative py-3 px-[2vw] md:w-[395px] bg-[#121212] border border-[#1e1e1e] rounded-lg">
                     <p className="text-gradient-orange rounded-lg inline-block px-5 font-medium text-center text-lg p-2 border border-[#1e1e1e] my-3">Standard</p>
                     <span>
-                        <Image src="/assets/light-ray.svg" className="absolute rounded-lg md:top-[15em] top-[14.7em] md:left-[45.5vw] left-[28vw]" alt="light-ray-effect" width={250} height={250} />
-                        <h3 className="text-white text-lg font-medium py-3">$500 /month</h3>
+                        <Image src="/assets/light-ray.svg" className="absolute rounded-lg top-0 right-0" alt="light-ray-effect" width={250} height={250} />
+                        {!yearly ? 
+                        <h3 className="text-white text-lg font-medium py-3">$500 /month</h3> : 
+                        <h3 className="text-white text-lg font-medium py-3">$15,000 /yearly</h3>}
                         <p className="text-[#929292] py-3">Advanced automation with custom workflows and integrations.</p>
                     </span>
                     <div className="flex flex-col">    
@@ -62,16 +81,20 @@ export default function Packages() {
                         <h3 className="text-white text-lg font-medium py-3">What's included:</h3>
                         {packages.filter((item) => item.type === "standard").map((item) => (
                             <span key={item.package_id} className="text-[#929292] py-3 text-wrap flex flex-row">
-                                <Image className="p-3 border border-[#1e1e1e]" src="/assets/checkmark.svg" alt="checkmark" width={40} height={40} />
+                                <div className="w-12 h-12 flex items-center justify-center shrink-0 border border-[#1e1e1e] rounded-md">
+                                    <Image src="/assets/checkmark.svg" alt="checkmark" width={20} height={20} />
+                                </div>
                                 <p className="px-3">{item.content}</p>
                             </span>
                         ))}
                     </span>
                 </div>
-                <div className="py-3 px-[2vw] mx-[3vw] bg-[#121212] border border-[#1e1e1e] rounded-lg">
+                <div className="py-3 px-[2vw] md:w-[395px] bg-[#121212] border border-[#1e1e1e] rounded-lg">
                     <p className="text-gradient-orange rounded-lg inline-block px-5 font-medium text-center text-lg p-2 border border-[#1e1e1e] my-3">Enterprise</p>
                     <span>
-                        <h3 className="text-white text-lg font-medium py-3">$1,199 /month</h3>
+                        {!yearly ? 
+                        <h3 className="text-white text-lg font-medium py-3">$1,199 /month</h3> : 
+                        <h3 className="text-white text-lg font-medium py-3">$15,000 /yearly</h3>}
                         <p className="text-[#929292] py-3">Fully customied automation built for complex business needs.</p>
                     </span>
                     <div className="flex flex-col">
@@ -84,7 +107,9 @@ export default function Packages() {
                         <h3 className="text-white text-lg font-medium py-3">What's included:</h3>
                         {packages.filter((item) => item.type === "enterprise").map((item) => (
                             <span key={item.package_id} className="text-[#929292] py-3 text-wrap flex flex-row">
-                                <Image className="p-3 border border-[#1e1e1e]" src="/assets/checkmark.svg" alt="checkmark" width={40} height={40} />
+                                <div className="w-12 h-12 flex items-center justify-center shrink-0 border border-[#1e1e1e] rounded-md">
+                                    <Image src="/assets/checkmark.svg" alt="checkmark" width={20} height={20} />
+                                </div>
                                 <p className="px-3">{item.content}</p>
                             </span>
                         ))}
